cd3b738e8214f5d167d35aeb0ef119c613ed10c9
Bhanuka Gamage <bhanukamgamage@gmail.com>
2018-09-10 20:28:40 +0800

    Created a script to capture the camera feed from webcam

--- capture.py
+++ capture.py
@@ -1,16 +1,42 @@
 # This project is to capture the web cam footage
 
-
-import cv2,time
+# Imprting the modules
+import cv2
 
 
 # Create an object for the camera. Zero for the external camera
 videoCamera = cv2.VideoCapture(0)
 
+# Initializing the frame count
+frameCount = 0
 
-print("OpenCV2")
+while True:
 
+    # Increasing the timer by one
+    frameCount = frameCount + 1
+
+    # Create a frame object
+    check, frame = videoCamera.read()
+
+    # Showing the matrix
+    print(frame)
+
+    # Display the frame
+    cv2.imshow("Capturing",frame)
+
+
+    # Wait for a key being pressed
+    key = cv2.waitKey(1)
+
+    # If the key is q then stop the loop
+    if key == ord("q"):
+        break
+
+# Print to total number of frames
+print(frameCount)
 
 # Shutdown the camera
 videoCamera.release()
 
+# Destroy all the windows
+cv2.destroyAllWindows()
