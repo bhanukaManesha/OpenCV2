ddbfaf487129ca1c41a60a4f98fbac82c27085ba
Bhanuka Gamage <bhanukamgamage@gmail.com>
2018-09-10 20:12:59 +0800

    Added Initial Camera Module

--- capture.py
+++ capture.py
@@ -1,3 +1,16 @@
 # This project is to capture the web cam footage
 
+
+import cv2,time
+
+
+# Create an object for the camera. Zero for the external camera
+videoCamera = cv2.VideoCapture(0)
+
+
 print("OpenCV2")
+
+
+# Shutdown the camera
+videoCamera.release()
+
